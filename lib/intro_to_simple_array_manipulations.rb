6e9d873d452b26412f8df813b1f6d187526c4cc6
Jakob Bellamy <jakbellamy@gmail.com>
2018-11-05 03:34:55 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,31 @@ end
 def pop_with_args(array)
   array.pop(2)
 end
+
+def using_shift
+  array.shift
+end
+
+def shift_with_args
+  array.shift(2)
+end
+
+def using_concat(array_1, array_2)
+  array_1.concat(array_2)
+end
+
+def using_insert(array, new_element)
+  array.insert(4, new_element)
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, int)
+  array.delete_at(int)
+end
